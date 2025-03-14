ebcc96f719a40dcf0681976813d401af3dbe504f
strahb <strobiisundae@gmail.com>
2025-03-14 21:20:55 +0000

    Some comments here and there, mostly figuring out why the code exports the wrong item even though it passes the correct parameters

--- Essence Management.lua
+++ Essence Management.lua
@@ -26,7 +26,7 @@ local monitor = peripheral.find("monitor") if peripheral.find("monitor") == nil 
         print("Monitor not found")
         print("Dashboard will not work but program will continue")
     else
-        monitor.setTextScale(1)
+        monitor.setTextScale(0.5)
         monitor.setCursorPos(1,1)
         monitor.clear()
         term.redirect(monitor)
@@ -55,9 +55,15 @@ end
 
 local function ExportItem(ItemID, Amount)
     -- Function to export item to the upper side of the bridge
-    formatted_query = {item=ItemID, count=Amount} --[[ Properly form the request as the bridge cannot look up a simple string
-    I could do it in a single line but it would make the code very hard to read ]] 
-    bridge.exportItem({formatted_query, "up"})
+    FormattedQuery = { item = ItemID, count = Amount } --[[ Format the Query
+        name: string	The registry name of the item
+        fingerprint: string?	A unique fingerprint which identifies the item to craft
+        amount: number	The amount of the item in the system
+        displayName: string	The display name for the item
+        isCraftable: boolean	Whether the item has a crafting pattern or not
+        nbt: string?	NBT to match the item on
+        tags: table	A list of all of the item tags ]]
+    bridge.exportItem(FormattedQuery, "up")
     print(string.format("Exported %d %s", Amount, ItemID))
 end
 
@@ -67,10 +73,16 @@ exportable_essences =   {"mysticalagriculture:inferium_essence",
                         "mysticalagriculture:imperium_essence",
                         "mysticalagriculture:supremium_essence"}
 
-for i, EssenceName in ipairs(exportable_essences) do
-    essence = GetItem(EssenceName)
+-- for i, EssenceName in ipairs(exportable_essences) do
+--     essence = GetItem(EssenceName)
+--     if essence ~= 0 then -- Execute the block as long as GetItem does not return "0" (Refer to line 52)
+--         print(essence.displayName)
+--         print(essence.amount)
+--     end
+-- end
+ExportItem("mysticalagriculture:inferium_essence", 256)
+essence = GetItem("mysticalagriculture:inferium_essence")
     if essence ~= 0 then -- Execute the block as long as GetItem does not return "0" (Refer to line 52)
         print(essence.displayName)
         print(essence.amount)
     end
-end
